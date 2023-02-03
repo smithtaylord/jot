d67687668aadf9b962e9259fed591956f56de4ff
Taylor Smith <smith.taylord@gmail.com>
2023-02-03 12:34:45 -0700

    all functions are declared, and all console log to the screen. Everything talks

--- app/Controllers/NotesController.js
+++ app/Controllers/NotesController.js
@@ -1,9 +1,45 @@
 import { notesService } from "../Services/NotesService.js";
 
+function _drawNotes() {
+    console.log('_drawNotes Works');
+}
+
+function _drawNote() {
+    console.log('_drawNotes Works');
+}
+
 export class NotesController {
 
     constructor() {
         console.log('Notes Constructor Working!');
+        // Testing that functions are hooked up can be removed late
+        _drawNote()
+        _drawNotes()
+        this.createNote()
+        this.setNote()
+        this.deleteNote()
+        this.saveNote()
 
     }
+
+    createNote() {
+        console.log('create note controller works');
+        notesService.createNote()
+    }
+
+    setNote() {
+        console.log('set note controller works');
+        notesService.setNote()
+    }
+
+    deleteNote() {
+        console.log('delete note controller works');
+        notesService.deleteNote()
+    }
+
+    saveNote() {
+        console.log('saveNote controller works');
+        notesService.saveNote()
+    }
+
 }
--- app/Services/NotesService.js
+++ app/Services/NotesService.js
@@ -1,6 +1,18 @@
 
 
 class NotesService {
+    saveNote() {
+        console.log('saveNote service works');
+    }
+    deleteNote() {
+        console.log('delete Note service works');
+    }
+    setNote() {
+        console.log('set Note service works');
+    }
+    createNote() {
+        console.log('create note service works');
+    }
     constructor() {
         console.log('Notes Service Working!');
     }
